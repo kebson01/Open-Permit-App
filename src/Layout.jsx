b968e905fef5e3dccefbcc2eabb5a6ee0edb4b99
Kebson St Charles <kebsonstcharles@gmail.com>
2026-03-09 16:32:14 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 
 const navLinks = [
   { name: "Home", page: "Home", icon: Building2 },
-  { name: "Interactive Map", page: "InteractiveMap", icon: Map },
+  { name: "Visual Permit Guide", page: "InteractiveMap", icon: Map },
   { name: "Fee Calculator", page: "FeeCalculator", icon: Calculator },
 ];
 
