59ae53ff697912ed9700bba317c935aef6ef2496
Kebson St Charles <kebsonstcharles@gmail.com>
2026-03-25 03:10:28 +0000

    File changes

--- src/components/map/HouseView.jsx
+++ src/components/map/HouseView.jsx
@@ -439,7 +439,7 @@ export default function HouseView({ view, showHighlights, onZoneClick }) {
         >
           <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
             <div style={{ ...zoomTransformStyle, position: "relative", width: "100%", height: "100%" }}>
-              <MapCanvas view={view} showHighlights={showHighlights} onZoneClick={onZoneClick} />
+              <MapCanvas view={view} showHighlights={showHighlights} onZoneClick={onZoneClick} zoneImages={zoneImages} onUploadRequest={handleUploadRequest} />
             </div>
           </div>
           <div className="absolute top-3 left-3 z-10 flex items-center gap-2">
